53b95f98a024da43abb49b26ed0089210e82282f
Łukasz Socha <kontakt@lukasz-socha.pl>
2020-10-23 13:02:04 +0200

    still optimise the queries

--- app/Console/Commands/SumLogs.php
+++ app/Console/Commands/SumLogs.php
@@ -44,6 +44,7 @@ class SumLogs extends Command
      */
     public function handle()
     {
+        $count = 0;
         $tables = $this->geLogTables();
         $tables=['access_logs_20201019'];
         $originals = collect([]);
@@ -60,21 +61,47 @@ class SumLogs extends Command
                 $accessLogs = collect([]);
                 $day = $this->getDateFromTableName($table);
                 if ($this->getDaysFromNow($day) > 1) {
-                    DB::table($table)->select('request', 'size')->orderBy('created_at')->chunk(1000, function ($logEntries) use (&$accessLogs, &$originals) {
+                    DB::table($table)->select('request', 'size')->orderBy('created_at')->chunk(1000, function ($logEntries) use (&$accessLogs, &$originals, &$count) {
                         foreach ($logEntries as $log) {
                             $filePath = $this->getFilePathFromLog($log->request);
-                            $accessLogs->put($filePath, $log);
+                            if($originals->has($filePath)) {
+                                $original = $originals->get($filePath);
+                                $this->info($log->request);
+                                if($accessLogs->has($original->id)) {
+                                    $accessLog = $accessLogs->get($original->id);
+                                    $logToCollection = [
+                                        'path' => $filePath,
+                                        'size' => $accessLog['size'] +  $log->size,
+                                        'amount' => $accessLog['amount'] + 1,
+
+                                    ];
+                                } else {
+                                    $logToCollection = [
+                                        'path' => $filePath,
+                                        'size' => $log->size,
+                                        'amount' => 1,
+                                    ];
+                                }
+                                $accessLogs->put($original->id, $logToCollection);
+                            }
                         }
                     });
+                    Log::debug($accessLogs->all());
+                    /*
+                    $accessLogsGrouped = $accessLogs->groupBy('path');
+                    Log::debug($accessLogsGrouped->all());
                     $originals->each(function ($original, $path) use (
                         $accessLogs
                     ) {
                         if($path === 'lukasz-socha.pl/wp-content/uploads/2014/03/slider2.jpg?il-height=75&il-avif=1') {
-                            $this->info($path);
-                            $accessLogsForPath = $accessLogs->keyBy($path);
-                            Log::debug($accessLogsForPath->all());
+                            //$this->info($path);
+                            if(!empty($accessLogsGrouped[$path])) {
+                                $accessLogsForPath = $accessLogsGrouped[$path];
+                                $this->info(count($accessLogsForPath));
+                            }
                         }
                     });
+                    */
                 }
             } catch (\Exception $e) {
                 $this->info($e->getMessage());
@@ -128,7 +155,7 @@ class SumLogs extends Command
         $logsTables = [];
         $allTables = array_map('reset', DB::select('SHOW TABLES'));
         foreach ($allTables as $table) {
-            if (strpos($table, 'access_logs_') !== false) {
+            if (Str::startsWith($table, 'access_logs_')) {
                 $logsTables[] = $table;
             }
         }
@@ -139,7 +166,7 @@ class SumLogs extends Command
     {
         $params = '';
         $parts = explode('/', $path);
-        if (!Str::startsWith($parts[0], '.')) {
+        if (!Str::contains($parts[0], '.')) {
             $params = '?' . $parts[0];
             array_shift($parts);
         }
@@ -152,7 +179,7 @@ class SumLogs extends Command
         if (empty($parts[1])) {
             throw new \Exception('A file path doesn\'t exist');
         }
-        return ltrim($parts[1], '/');
+        return Str::before(ltrim($parts[1], '/'), '?');
     }
 
     private function getSumFromEntries($entries)
